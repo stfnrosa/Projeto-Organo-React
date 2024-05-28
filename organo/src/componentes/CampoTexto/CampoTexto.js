d2aa75a5bdc06e7ef5711e538fa2381673b5208c
stfnrosa <rosaa.estefanii@gmail.com>
2024-05-28 19:54:06 -0300

    implementando com useState

--- organo/src/componentes/CampoTexto/CampoTexto.js
+++ organo/src/componentes/CampoTexto/CampoTexto.js
@@ -1,10 +1,18 @@
+import {useState} from 'react'
 import './CampoTexto.css'
 
 const CampoTexto = (props)  => {
+
+    const [valor, setValor] = useState('')
+
+    const aoDigitar = (evento) => {
+        props.aoAlterado(evento.target.value)
+    }
+
     return (
         <div className="campo-texto">
             <label>{props.label}</label>
-            <input placeholder={props.placeholder}/>
+            <input value={props.valor} onChange={aoDigitar} required={props.obrigatorio} placeholder={props.placeholder}/>
         </div>
     )
 }
